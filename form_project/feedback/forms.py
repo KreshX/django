79c8d022ea196aa57e8a1a1c7befb6a64fd87f02
Egor Kroshkin <kroshkin.egor@yandex.ru>
2022-07-08 22:54:50 +0300

    after74

--- form_project/feedback/forms.py
+++ form_project/feedback/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class FeedbackForm(forms.Form):
-	name = forms.CharField()
-	# surname = forms.CharField()
-	# feedback = forms.CharField(widget=forms.Textarea(attrs={'rows':2, 'cols':20}))
-	rating = forms.IntegerField(min_value=1, max_value=10)
+	name = forms.CharField(label='Имя')
+	surname = forms.CharField(label='Фамилия')
+	feedback = forms.CharField(label='Отзыв', widget=forms.Textarea(attrs={'rows':2, 'cols':20}))
+	rating = forms.IntegerField(label='Рейтинг', min_value=1, max_value=10)
--- form_project/feedback/models.py
+++ form_project/feedback/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Feedback(models.Model):
+	name = models.CharField(max_length=40)
+	surname = models.CharField(max_length=40)
+	feedback = models.TextField()
+	rating = models.PositiveIntegerField()
