5188724eca468be99af539661495fa9bb54b39d3
Egor Kroshkin <kroshkin.egor@yandex.ru>
2022-07-08 21:31:36 +0300

    after73

--- form_project/feedback/forms.py
+++ form_project/feedback/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 class FeedbackForm(forms.Form):
 	name = forms.CharField()
-	surname = forms.CharField()
-	feedback = forms.CharField(widget=forms.Textarea(attrs={'rows':2, 'cols':20}))
+	# surname = forms.CharField()
+	# feedback = forms.CharField(widget=forms.Textarea(attrs={'rows':2, 'cols':20}))
+	rating = forms.IntegerField(min_value=1, max_value=10)
